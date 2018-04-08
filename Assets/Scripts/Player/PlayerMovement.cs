baaeb8281984f76f2c94106a904479d87caa6fbe
Zee <zee_shan265@hotmail.com>
2018-04-07 20:06:33 -0400

    player moving and facing all directions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,8 +7,12 @@ using UnityEngine;
 	public Color playerColor;
 	public float playerHealth;
 	public float playerSpeed;
+	public Transform bulletSpawner;
+	public GameObject bullet;
+	public string facingDirection;
 	public float fireRate;
 	private bool isDead = false;
+	public Animator animator;
 }
 
 public class PlayerMovement : MonoBehaviour {
@@ -20,19 +24,70 @@ public class PlayerMovement : MonoBehaviour {
 	void Awake() {
 		m_meshRenderer = GetComponent<MeshRenderer>();
 		m_rigidbody = GetComponent<Rigidbody>();
+		m_playerAttr.animator = GetComponent<Animator>();
 
 		m_playerAttr.playerColor = m_meshRenderer.material.color;
 		m_playerAttr.playerHealth = 100;
+		m_playerAttr.facingDirection = "Right";
 	}
 
 	void Update() {
+		
 		if(m_playerAttr.playerName == "Player1") {
-			float moveHorizontal = Input.GetAxis("Horizontal");
-			float moveVertical = Input.GetAxis("Vertical");
+			float moveHorizontal = Input.GetAxisRaw("Horizontal");
+			float moveVertical = Input.GetAxisRaw("Vertical");
 
 			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-			m_rigidbody.velocity = movement * m_playerAttr.playerSpeed;
+			m_rigidbody.velocity = movement.normalized * m_playerAttr.playerSpeed;
+			
+			// Debug.Log(movement);
+			if(movement.x < 0) { m_playerAttr.facingDirection = "Left"; }
+			if(movement.x > 0) { m_playerAttr.facingDirection = "Right"; }
+			if(movement.z < 0) { m_playerAttr.facingDirection = "Down"; }
+			if(movement.z > 0) { m_playerAttr.facingDirection = "Up"; }
+
+			if(movement.z > 0 && movement.x > 0) { // UP RIGHT
+				m_playerAttr.facingDirection = "Up Right";
+				m_playerAttr.animator.SetFloat("UpDown", 0.5f);
+				m_playerAttr.animator.SetFloat("LeftRight", 0.5f);
+			}
+			if(movement.z > 0 && movement.x < 0) { // UP LEFT
+				m_playerAttr.facingDirection = "Up Left";
+				m_playerAttr.animator.SetFloat("UpDown", -0.5f);
+				m_playerAttr.animator.SetFloat("LeftRight", 0.5f);
+			}
+			if(movement.z < 0 && movement.x > 0) { // DOWN RIGHT
+				m_playerAttr.facingDirection = "Down Right";
+				m_playerAttr.animator.SetFloat("UpDown", 0.5f);
+				m_playerAttr.animator.SetFloat("LeftRight", -0.5f);
+			}
+			if(movement.z < 0 && movement.x < 0) { // DOWN LEFT
+				m_playerAttr.facingDirection = "Down Left";
+				m_playerAttr.animator.SetFloat("UpDown", -0.5f);
+				m_playerAttr.animator.SetFloat("LeftRight", -0.5f);
+			}
+
+			// if(Input.GetKey(KeyCode.Space)) {
+			// 	GameObject bullet = Instantiate(m_playerAttr.bullet, m_playerAttr.bulletSpawner.position, Quaternion.identity);
+			// }
+		}
+
+		if(m_playerAttr.facingDirection == "Left") { 
+			m_playerAttr.animator.SetFloat("UpDown", -1);
+			m_playerAttr.animator.SetFloat("LeftRight", 0);
+		}
+		if(m_playerAttr.facingDirection == "Right") { 
+			m_playerAttr.animator.SetFloat("UpDown", 1);
+			m_playerAttr.animator.SetFloat("LeftRight", 0);
+		}
+		if(m_playerAttr.facingDirection == "Down") { 
+			m_playerAttr.animator.SetFloat("UpDown", 0);
+			m_playerAttr.animator.SetFloat("LeftRight", -1);
+		}
+		if(m_playerAttr.facingDirection == "Up") { 
+			m_playerAttr.animator.SetFloat("UpDown", 0);
+			m_playerAttr.animator.SetFloat("LeftRight", 1);
 		}
 	}
 
